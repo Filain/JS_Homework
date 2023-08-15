924df6a82cc335cded0e3c9b5ea13afc5acda46f
Filain <ratecurrent@gmail.com>
2023-08-15 23:06:21 +0300

    Lesson 1

---
+++ main.js
@@ -0,0 +1,60 @@
+// ----------------Task1--------------------
+
+let s1 = 'hello';
+let s2 = 'owu';
+let s3 = 'com';
+let s4 = 'ua';
+let n1=1;
+let n2=10;
+let n3=-999;
+let n4=123;
+let n5=3.14;
+let n6=2.7;
+let n7=16;
+let b1=true;
+let b2=false;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+console.log(b1);
+console.log(b2);
+
+// ----------------Task2--------------------
+
+let firstName='Volodymyr';
+let middleName='Igorovych';
+let lastName='Fylypiv';
+let person= firstName+' '+middleName+' '+lastName;
+console.log(person)
+
+// ----------------Task3--------------------
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+
+// ----------------Task4--------------------
+
+let firstName1=prompt('Enter First Name');
+let middleName1=prompt('Enter midd Name');
+let age=prompt('Enter Your age');
+let person1=firstName1+' '+middleName1+' '+age;
+console.log(person1);
+
+
+
+
+
